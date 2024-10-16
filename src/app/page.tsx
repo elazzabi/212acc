58cc99f730cab8f9d546b80b4302208a27c0c09f
Ahmed <ahmed.el.azzabi@automattic.com>
2024-10-16 11:58:01 -0500

    comment non necessary components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,47 +17,11 @@ import avatarImage2 from '@/images/avatars/avatar-2.png'
 export default function Home() {
   return (
     <>
-      {/* <Hero /> */}
       <Introduction />
-      <NavBar />
+      {/* <NavBar />
       <TableOfContents />
-      {/* */}
-      {/*  */}
-      {/* <Testimonial
-        id="testimonial-from-tommy-stroman"
-        author={{
-          name: 'Tommy Stroman',
-          role: 'Front-end developer',
-          image: avatarImage1,
-        }}
-      >
-        <p>
-          “I didn’t know a thing about icon design until I read this book. Now I
-          can create any icon I need in no time. Great resource!”
-        </p>
-      </Testimonial> */}
-      {/* <Screencasts />
-      <Testimonial
-        id="testimonial-from-gerardo-stark"
-        author={{
-          name: 'Gerardo Stark',
-          role: 'Creator of Pandemicons',
-          image: avatarImage2,
-        }}
-      >
-        <p>
-          “I’ve tried to create my own icons in the past but quickly got
-          frustrated and gave up. Now I sell my own custom icon sets online.”
-        </p>
-      </Testimonial>
-      <FreeChapters />
-      <Pricing />
-      <Friends /> */}
       <Testimonials />
-
-      <Resources />
-      {/* 
-      <Author /> */}
+      <Resources /> */}
       <Footer />
     </>
   )
